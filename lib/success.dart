062140b6a6e6edfbf298ae39f2b67be4921f6e04
Aadit Antony <aaditantony1112@gmail.com>
2025-07-10 18:33:01 +0530

    got the trial execution

---
+++ lib/success.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Success extends StatelessWidget {
+  const Success({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("suceessfully navigated to another page")),
+    );
+  }
+}
